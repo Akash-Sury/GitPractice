ceb1996dcafea7a2ffb7af761d175cfe87608a64
Akash-Sury <akashsury28695@gmail.com>
2021-07-23 01:06:44 +0530

    First Commit

---
+++ GitPractice/src/Arrays.java
@@ -0,0 +1,16 @@
+
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		String [] names= {"Akash","Jalindar","Suryawanshi"};
+		for(int i =0;i<names.length;i++) {
+			System.out.println(names[i]);
+			
+		}
+		
+
+	}
+
+}
