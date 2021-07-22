70164cdf59c76f5b66ee5d811646b44ab4cb5b17
Akash-Sury <akashsury28695@gmail.com>
2021-07-23 01:14:08 +0530

    One more class Added

--- GitPractice/src/Arrays1.java
+++ GitPractice/src/Arrays1.java
@@ -2,7 +2,16 @@
 public class Arrays1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
+		String [] names= {"Akash","Jalindar","Suryawanshi","Vadiye Raibag"};
+		for(int i =0;i<names.length;i++) {
+			System.out.println(names[i]);
+			
+		}
+		
+		
+		
 
 	}
 
